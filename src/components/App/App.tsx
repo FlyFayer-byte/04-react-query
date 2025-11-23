3f207953a437869fcb0dd8c1c364625411c9319a
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-11-23 22:49:12 +0100

    modified:   src/components/App/App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import ReactPaginate from 'react-paginate';
 
@@ -12,7 +12,7 @@ import type { Movie } from '../../types/movie';
 import type { TmdbSearchResponse } from '../../services/movieService';
 import { fetchMovies } from '../../services/movieService';
 
-import toast, { Toaster } from 'react-hot-toast';
+// import toast, { Toaster } from 'react-hot-toast';
 import styles from './App.module.css';
 
 export default function App() {
@@ -20,10 +20,10 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const toastShownRef = useRef({
-    noResults: false,
-    error: false,
-  });
+  // const toastShownRef = useRef({
+  //   noResults: false,
+  //   error: false,
+  // });
 
   const { data, isLoading, isError } = useQuery<TmdbSearchResponse>({
     queryKey: ['movies', search, page],
@@ -61,32 +61,32 @@ export default function App() {
 useEffect(() => {
   if (!search || isLoading) return;
 
-  if (isError && !toastShownRef.current.error) {
-    toast.error('Something went wrong while fetching movies.');
-    toastShownRef.current.error = true;
-    toastShownRef.current.noResults = false;
-  }
+  // if (isError && !toastShownRef.current.error) {
+  //   toast.error('Something went wrong while fetching movies.');
+  //   toastShownRef.current.error = true;
+  //   toastShownRef.current.noResults = false;
+  // }
 
-  if (data && data.results.length === 0 && !toastShownRef.current.noResults) {
-    toast.error('No movies found for your request.');
-    toastShownRef.current.noResults = true;
-  }
+  // if (data && data.results.length === 0 && !toastShownRef.current.noResults) {
+  //   toast.error('No movies found for your request.');
+  //   toastShownRef.current.noResults = true;
+  // }
 
-  if (data && data.results.length > 0) {
-    toastShownRef.current = { noResults: false, error: false };
-  }
+  // if (data && data.results.length > 0) {
+  //   toastShownRef.current = { noResults: false, error: false };
+  // }
 }, [isError, isLoading, data, search]);
 
   return (
     <div className={styles.app}>
-      <Toaster position="top-center" />
+      {/* <Toaster position="top-center" /> */}
 
       <SearchBar
         onSubmit={query => {
           setSearch(query);
           setPage(1);
 
-          toastShownRef.current = { noResults: false, error: false };
+          // toastShownRef.current = { noResults: false, error: false };
         }}
       />
 
